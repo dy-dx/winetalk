384037727a56b54f9b8c2378aaf09ee11ec62582
Chris Kim <christopherjkim@gmail.com>
2017-08-04 01:59:14 -0400

    thanks jordan

--- main.c
+++ main.c
@@ -1,25 +1,17 @@
-// jordan why
-// #define WIN32_LEAN_AND_MEAN
-// #define VC_EXTRALEAN
+
+#define WIN32_LEAN_AND_MEAN
+#define VC_EXTRALEAN
 
 #include <stdio.h>
 #include <windows.h>
 #include <mmsystem.h>   // includes multimedia extensions
 
-// I guess I don't need these
-
-// #include <string.h>     // includes string functions
-// #include <commdlg.h>    // includes common dialog functionality
-// #include <dlgs.h>       // includes common dialog template defines
-// #include <cderr.h>      // includes the common dialog error codes
-// #include <ctype.h>
-
-static UINT (*TextToSpeechStartLang)(LPSTR lang);
-static BOOL (*TextToSpeechSelectLang)(VOID *handle, UINT lang);
-static MMRESULT (*TextToSpeechSync)(VOID *handle);
-static MMRESULT (*TextToSpeechShutdown)(VOID *handle);
-static MMRESULT (*TextToSpeechSpeak)(VOID *handle, LPSTR msg, UINT flags);
-static MMRESULT (*TextToSpeechStartupEx)(
+typedef UINT (WINAPI TextToSpeechStartLangFn)(LPSTR lang);
+typedef BOOL (WINAPI TextToSpeechSelectLangFn)(VOID *handle, UINT lang);
+typedef MMRESULT (WINAPI TextToSpeechSyncFn)(VOID *handle);
+typedef MMRESULT (WINAPI TextToSpeechShutdownFn)(VOID *handle);
+typedef MMRESULT (WINAPI TextToSpeechSpeakFn)(VOID *handle, LPSTR msg, UINT flags);
+typedef MMRESULT (WINAPI TextToSpeechStartupExFn)(
     VOID **handle,
     UINT uiDeviceNumber,
     UINT dwDeviceOptions,
@@ -27,16 +19,23 @@ static MMRESULT (*TextToSpeechStartupEx)(
     LONG dwCallbackParameter
 );
 
+static TextToSpeechStartLangFn* TextToSpeechStartLang;
+static TextToSpeechSelectLangFn* TextToSpeechSelectLang;
+static TextToSpeechStartupExFn* TextToSpeechStartupEx;
+static TextToSpeechShutdownFn* TextToSpeechShutdown;
+static TextToSpeechSpeakFn* TextToSpeechSpeak;
+static TextToSpeechSyncFn* TextToSpeechSync;
+
 static UINT speakFlagNormal = 0;
 static UINT speakFlagForce = 1;
 
 void link_that_shit_homie(HMODULE h) {
-    TextToSpeechStartLang = (void *)GetProcAddress(h, "TextToSpeechStartLangA");
-    TextToSpeechSelectLang = (void *)GetProcAddress(h, "TextToSpeechSelectLang");
-    TextToSpeechStartupEx = (void *)GetProcAddress(h, "TextToSpeechStartupEx");
-    TextToSpeechShutdown = (void *)GetProcAddress(h, "TextToSpeechShutdown");
-    TextToSpeechSpeak = (void *)GetProcAddress(h, "TextToSpeechSpeakA");
-    TextToSpeechSync = (void *)GetProcAddress(h, "TextToSpeechSync");
+    TextToSpeechStartLang  = (TextToSpeechStartLangFn*)GetProcAddress(h, "TextToSpeechStartLangA");
+    TextToSpeechSelectLang = (TextToSpeechSelectLangFn*)GetProcAddress(h, "TextToSpeechSelectLang");
+    TextToSpeechStartupEx  = (TextToSpeechStartupExFn*)GetProcAddress(h, "TextToSpeechStartupEx");
+    TextToSpeechShutdown   = (TextToSpeechShutdownFn*)GetProcAddress(h, "TextToSpeechShutdown");
+    TextToSpeechSpeak      = (TextToSpeechSpeakFn*)GetProcAddress(h, "TextToSpeechSpeakA");
+    TextToSpeechSync       = (TextToSpeechSyncFn*)GetProcAddress(h, "TextToSpeechSync");
 }
 
 // might need this later
@@ -52,10 +51,10 @@ int main(int argc, char* argv[])
         msg = argv[1];
     }
 
-    HMODULE dllHandle = LoadLibrary("FonixTalk.dll");
+    HMODULE dllHandle = LoadLibrary(TEXT("FonixTalk.dll"));
     if (dllHandle == NULL)
     {
-        exit(1);
+        return 1;
     }
 
     link_that_shit_homie(dllHandle);
@@ -75,6 +74,5 @@ int main(int argc, char* argv[])
     mmStatus = TextToSpeechShutdown(_handle);
 
     FreeLibrary(dllHandle);
-    exit(0); // idk it breaks if I don't do this
     return 0;
 }
