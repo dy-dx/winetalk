3bd17f4d4517f5381660e77d55ce5111c1a723a8
Chris Kim <christopherjkim@gmail.com>
2017-08-04 04:18:19 -0400

    it streams to stdout

---
+++ build-and-test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+winegcc -m32 -mconsole main.c -o winetalk.sh \
+  && WINEDEBUG=-all ./winetalk.sh "hello world hello world" \
+    | play -b 16 -e signed-integer -L -r 11025 -c 1 -t raw -
--- main.c
+++ main.c
@@ -6,11 +6,48 @@
 #include <windows.h>
 #include <mmsystem.h>   // includes multimedia extensions
 
+typedef struct TTS_PHONEME_TAG {
+    DWORD dwPhoneme;
+    DWORD dwPhonemeSampleNumber;
+    DWORD dwPhonemeDuration;
+    DWORD dwReserved;
+} TTS_PHONEME_T;
+
+typedef TTS_PHONEME_T * LPTTS_PHONEME_T;
+
+typedef struct TTS_INDEX_TAG {
+    DWORD dwIndexValue;
+    DWORD dwIndexSampleNumber;
+    DWORD dwReserved;
+} TTS_INDEX_T;
+
+typedef TTS_INDEX_T * LPTTS_INDEX_T;
+
+typedef struct TTS_BUFFER_TAG
+{
+    LPSTR lpData;
+    LPTTS_PHONEME_T lpPhonemeArray;
+    LPTTS_INDEX_T lpIndexArray;
+
+    DWORD dwMaximumBufferLength;
+    DWORD dwMaximumNumberOfPhonemeChanges;
+    DWORD dwMaximumNumberOfIndexMarks;
+    DWORD dwBufferLength;
+    DWORD dwNumberOfPhonemeChanges;
+    DWORD dwNumberOfIndexMarks;
+    DWORD dwReserved;
+} TTS_BUFFER_T;
+
+typedef TTS_BUFFER_T * LPTTS_BUFFER_T;
+
+
 typedef UINT (WINAPI TextToSpeechStartLangFn)(LPSTR lang);
 typedef BOOL (WINAPI TextToSpeechSelectLangFn)(VOID *handle, UINT lang);
 typedef MMRESULT (WINAPI TextToSpeechSyncFn)(VOID *handle);
 typedef MMRESULT (WINAPI TextToSpeechShutdownFn)(VOID *handle);
 typedef MMRESULT (WINAPI TextToSpeechSpeakFn)(VOID *handle, LPSTR msg, UINT flags);
+typedef MMRESULT (WINAPI TextToSpeechAddBufferFn)(VOID *handle, TTS_BUFFER_T *buffer);
+typedef MMRESULT (WINAPI TextToSpeechOpenInMemoryFn)(VOID *handle, UINT format);
 typedef MMRESULT (WINAPI TextToSpeechStartupExFn)(
     VOID **handle,
     UINT uiDeviceNumber,
@@ -25,22 +62,67 @@ static TextToSpeechStartupExFn* TextToSpeechStartupEx;
 static TextToSpeechShutdownFn* TextToSpeechShutdown;
 static TextToSpeechSpeakFn* TextToSpeechSpeak;
 static TextToSpeechSyncFn* TextToSpeechSync;
+static TextToSpeechAddBufferFn* TextToSpeechAddBuffer;
+static TextToSpeechOpenInMemoryFn* TextToSpeechOpenInMemory;
+
+void link_that_shit_homie(HMODULE h)
+{
+    TextToSpeechStartLang    = (TextToSpeechStartLangFn*)GetProcAddress(h, "TextToSpeechStartLangA");
+    TextToSpeechSelectLang   = (TextToSpeechSelectLangFn*)GetProcAddress(h, "TextToSpeechSelectLang");
+    TextToSpeechStartupEx    = (TextToSpeechStartupExFn*)GetProcAddress(h, "TextToSpeechStartupEx");
+    TextToSpeechShutdown     = (TextToSpeechShutdownFn*)GetProcAddress(h, "TextToSpeechShutdown");
+    TextToSpeechSpeak        = (TextToSpeechSpeakFn*)GetProcAddress(h, "TextToSpeechSpeakA");
+    TextToSpeechSync         = (TextToSpeechSyncFn*)GetProcAddress(h, "TextToSpeechSync");
+    TextToSpeechAddBuffer    = (TextToSpeechAddBufferFn*)GetProcAddress(h, "TextToSpeechAddBuffer");
+    TextToSpeechOpenInMemory = (TextToSpeechOpenInMemoryFn*)GetProcAddress(h, "TextToSpeechOpenInMemory");
+}
+
+UINT UiIndexMsg;
+UINT UiErrorMsg;
+UINT UiBufferMsg;
+UINT UiPhonemeMsg;
+
+void register_window_messages()
+{
+    UiIndexMsg = RegisterWindowMessage("DECtalkIndexMessage");
+    UiErrorMsg = RegisterWindowMessage("DECtalkErrorMessage");
+    UiBufferMsg = RegisterWindowMessage("DECtalkBufferMessage");
+    UiPhonemeMsg = RegisterWindowMessage("DECtalkVisualMessage");
+}
 
 static UINT speakFlagNormal = 0;
 static UINT speakFlagForce = 1;
+// signed 16bit little endian 11025hz mono PCM
+static UINT WaveFormat_1M16 = 0x00000004;
 
-void link_that_shit_homie(HMODULE h) {
-    TextToSpeechStartLang  = (TextToSpeechStartLangFn*)GetProcAddress(h, "TextToSpeechStartLangA");
-    TextToSpeechSelectLang = (TextToSpeechSelectLangFn*)GetProcAddress(h, "TextToSpeechSelectLang");
-    TextToSpeechStartupEx  = (TextToSpeechStartupExFn*)GetProcAddress(h, "TextToSpeechStartupEx");
-    TextToSpeechShutdown   = (TextToSpeechShutdownFn*)GetProcAddress(h, "TextToSpeechShutdown");
-    TextToSpeechSpeak      = (TextToSpeechSpeakFn*)GetProcAddress(h, "TextToSpeechSpeakA");
-    TextToSpeechSync       = (TextToSpeechSyncFn*)GetProcAddress(h, "TextToSpeechSync");
+UINT uiDeviceNumber = 0xFFFFFFFF;
+UINT dwDeviceOptions = 0;
+
+VOID *_handle = NULL;
+TTS_BUFFER_T _buffer = {};
+#define BUFFER_SIZE 16384
+char daBytes[BUFFER_SIZE];
+
+void ResetBuffer()
+{
+    _buffer.dwBufferLength = 0;
+    _buffer.dwNumberOfPhonemeChanges = 0;
+    _buffer.dwNumberOfIndexMarks = 0;
+    // I don't think I need to do this but whatevs
+    memset(&daBytes[0], 0, BUFFER_SIZE);
 }
 
-// might need this later
 void TTSCallbackRoutine(LONG lParam1, LONG lParam2, DWORD dwInstanceParam, UINT uiMsg)
 {
+    if (uiMsg == UiBufferMsg)
+    {
+        UINT length = _buffer.dwBufferLength;
+        if(fwrite(daBytes, sizeof(char), length, stdout) != length) {
+            perror("fwrite");
+        }
+        ResetBuffer();
+        TextToSpeechAddBuffer(_handle, &_buffer);
+    }
 }
 
 int main(int argc, char* argv[])
@@ -58,18 +140,19 @@ int main(int argc, char* argv[])
     }
 
     link_that_shit_homie(dllHandle);
-    VOID *_handle = NULL;
-    UINT uiDeviceNumber = 0xFFFFFFFF;
-    UINT dwDeviceOptions = 0;
+    register_window_messages();
+
+    _buffer.dwMaximumBufferLength = BUFFER_SIZE;
+    _buffer.lpData = &daBytes[0];
+
     MMRESULT mmStatus;
 
     unsigned int langid = TextToSpeechStartLang("US");
     TextToSpeechSelectLang(NULL, langid);
-
-    mmStatus = TextToSpeechStartupEx(&_handle, uiDeviceNumber, dwDeviceOptions, NULL, 0);
-
+    mmStatus = TextToSpeechStartupEx(&_handle, uiDeviceNumber, dwDeviceOptions, TTSCallbackRoutine, 0);
+    mmStatus = TextToSpeechOpenInMemory(_handle, WaveFormat_1M16);
+    mmStatus = TextToSpeechAddBuffer(_handle, &_buffer);
     mmStatus = TextToSpeechSpeak(_handle, msg, speakFlagForce);
-
     mmStatus = TextToSpeechSync(_handle);
     mmStatus = TextToSpeechShutdown(_handle);
 
